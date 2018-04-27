b9067a3b35da9ef468ca07920d6971bee2f19a85
Giandomenico Olini <giando.olini@gogodigital.it>
2018-04-27 13:26:55 +0200

    Separate views

---
+++ views/default/_navbar.php
@@ -0,0 +1,7 @@
+<div class="row">
+	<div class="col-md-12">
+		<a class="btn btn-default" role="button" data-toggle="collapse" href="#collapseMedia" aria-expanded="false" aria-controls="collapseMedia">
+			<?= Yii::t('media','Add Media') ?>
+		</a>
+	</div>
+</div>
--- views/default/index.php
+++ views/default/index.php
@@ -9,13 +9,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<div class="row">
-	<div class="col-md-12">
-		<a class="btn btn-default" role="button" data-toggle="collapse" href="#collapseMedia" aria-expanded="false" aria-controls="collapseMedia">
-			<?= Yii::t('media','Add Media') ?>
-		</a>
-	</div>
-</div>
+<?= Yii::$app->view->renderFile('@vendor/cinghie/yii2-media/views/default/_navbar.php'); ?>
 
 <div class="row">
 	<div class="col-md-12">
