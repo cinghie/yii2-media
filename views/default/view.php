421ca2a3936c4eeb150d8fcb88968d0616e0926b
Giandomenico Olini <giando.olini@gogodigital.it>
2020-02-06 21:39:40 +0100

    Fix views

--- views/default/view.php
+++ views/default/view.php
@@ -16,6 +16,12 @@ $this->registerCss('
         max-width: 100%;
         min-height: 350px;
     }
+    .modal-image i {
+        font-size: 200px;
+        margin: 0 auto;
+        padding: 10px 50px;
+        text-align: center;
+    }
     .modal-image img {
         padding: 10px 50px;
     }
@@ -33,7 +39,6 @@ $this->registerCss('
 <div class="row">
 
 	<div class="col-sm-7 modal-image">
-
 		<?php
 
 		if (strpos($model->mimetype, 'video') !== false) {
@@ -41,18 +46,15 @@ $this->registerCss('
 		} else {
 			echo $model->getMediaThumbsUrl();
         } ?>
-
 	</div>
 
 	<div class="col-sm-5 modal-info" style="font-size: 14px; margin-top: 45px;">
-
         <span class="modal-title"><strong><?= Yii::t('traits','Title') ?></strong>: <?= $model->title ?></span><br>
         <span class="modal-filename"><strong><?= Yii::t('traits','Filename') ?></strong>: <?= $model->filename ?></span><br>
         <span class="modal-mymetype"><strong><?= Yii::t('traits','MimeType') ?></strong>: <?= $model->mimetype ?></span><br>
         <span class="modal-created"><strong><?= Yii::t('traits','Created') ?></strong>: <?= $model->created ?></span><br>
         <span class="modal-createdby"><strong><?= Yii::t('traits','Created By') ?></strong>: <?= $model->createdBy->username ?></span><br>
         <span class="modal-size"><strong><?= Yii::t('traits','Size') ?></strong>: <?= $model->formatSize() ?></span><br>
-
 	</div>
 
 </div>
